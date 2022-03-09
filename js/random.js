ed31bd07248148bff45f60beba3d26710e078fda
tonicraft18 <cmine726@gmail.com>
2022-03-09 09:34:31 +0100

    fixing onload

--- js/random.js
+++ js/random.js
@@ -1,4 +1,4 @@
-window.onload = () => {
+window.addEventListener("load",() => {
   var frase = ["Puro entretenimiento :)","Dale en <a href=\"https://github.com/tonicraft18\" style=\"text-decoration: none\">mi GitHub :D"+
   "</a>","Junior Programmer","S&iacute;gueme tambi&eacute;n en twitter uwu","Buena m&uacute;sica&nbsp;&iquest;no bro?","Web Deevelopeeer",
   "Música: Casin - glue70","Ingeniero en camino :D","Periodic Hour?","uwu","Energy = Much * Coffe^2","idk what put here","git commit -m "+
@@ -13,4 +13,4 @@ window.onload = () => {
   num = parseInt(num)
   document.getElementById("rand").innerHTML = frase[num];
   console.log(frase.length + " / " + num)
-}
+})
