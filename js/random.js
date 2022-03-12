531a94d7380548f47f9d9dc15444f3f3ad230dcc
tonicraft18 <cmine726@gmail.com>
2022-03-12 19:25:21 +0100

    all

--- js/random.js
+++ js/random.js
@@ -1,6 +1,5 @@
 console.error("HTTPS error, given by matrenas@tcd.org")
-
-function asd(){
+window.onload = () => {
   var frase = ["Puro entretenimiento :)","Dale en <a href=\"https://github.com/tonicraft18\" style=\"text-decoration: none\">mi GitHub :D"+
   "</a>","Junior Programmer","S&iacute;gueme tambi&eacute;n en twitter uwu","Buena m&uacute;sica&nbsp;&iquest;no bro?","Web Deevelopeeer",
   "Música: Casin - glue70","Ingeniero en camino :D","Periodic Hour?","uwu","Energy = Much * Coffe^2","idk what put here","git commit -m "+
